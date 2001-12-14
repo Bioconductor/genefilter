8d988673fd81aeba8f6cc0e29a1dd612104bdb17
Vince Carey <stvjc@channing.harvard.edu>
2001-12-14 22:53:47 +0000

    much new
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genefilter@269 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,17 @@
-.First.lib <- function(lib, pkg) library.dynam("genefilter", pkg, lib)
+
+.First.lib <- function(lib, pkgname, where) 
+  {
+  library.dynam("genefilter", pkgname, lib)
+  require(methods)
+    if(missing(where)) {
+        where <- match(paste("package:", pkgname, sep=""), search())
+        if(is.na(where)) {
+            warning(paste("Not a package name: ",pkgname))
+            return()
+        }
+        where <- pos.to.env(where)
+    }
+  .initRank(where)
+  cacheMetaData(as.environment(where))
+  }
+
