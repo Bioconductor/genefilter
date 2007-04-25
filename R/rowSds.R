3d5c7f72c128e6ebd64cbe3a4c868018d713549b
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-04-25 16:52:21 +0000

    added rowVars
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genefilter@24271 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rowSds.R
+++ R/rowSds.R
@@ -1,6 +1,9 @@
-rowSds <- function(x, ...) {
+rowVars = function(x, ...) {
   sqr     = function(x)  x*x
   n       = rowSums(!is.na(x))
   n[n<=1] = NA
-  return(sqrt(rowSums(sqr(x-rowMeans(x, ...)), ...)/(n-1)))
+  return(rowSums(sqr(x-rowMeans(x, ...)), ...)/(n-1))
 }
+
+rowSds = function(x, ...)
+  sqrt(rowVars(x, ...))
