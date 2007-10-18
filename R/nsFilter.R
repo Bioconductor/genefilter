11fc1f6159dfb59a34ec7f6a4151c358547a9784
Robert Gentleman <rgentlem@fhcrc.org>
2007-10-18 05:05:30 +0000

    fixed nsFilter so it does what it is documented to do, and added varFilter and featureFilter that separate out the tasks - sooner or later we will change nsFilter so it just uses these two helper functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genefilter@28054 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/nsFilter.R
+++ R/nsFilter.R
@@ -1,3 +1,92 @@
+##RG introduces two new functions, varFilter that does what nsFilter
+##was supposed to, but never did, and featureFilter that does the only
+##useful stuff that nsFilter does
+rowIQRs = function(eSet) {
+  numSamp = ncol(eSet)
+  lowQ = rowQ(eSet, floor(0.25 * numSamp))
+  upQ = rowQ(eSet, ceiling(0.75 * numSamp))
+  upQ - lowQ
+}
+
+varFilter <- function(eset, var.func=IQR, var.cutoff=0.5)
+{
+    if( deparse(substitute(var.func)) == "IQR") {
+        vars = rowIQRs(eset)
+    } else {
+        vars <- apply(exprs(eset), 1, var.func)
+    }
+    if( var.cutoff > 1 || var.cutoff < 0 )
+        selected <- vars > var.cutoff
+    else {
+        quant = quantile(vars, probs = var.cutoff)
+        selected = vars > quant
+    }
+    eset <- eset[selected, ]
+}
+
+featureFilter <- function(eset, require.entrez=TRUE,
+                   require.GOBP=FALSE, require.GOCC=FALSE,
+                   require.GOMF=FALSE, remove.dupEntrez=TRUE,
+                   feature.exclude="^AFFX") {
+
+    annChip <- annotation(eset)
+    if (nchar(annChip) == 0) stop("'eset' must have a valid annotation slot")
+    getAnnEnv <- function(map) { getAnnMap(map=map, chip=annChip) }
+    
+    nfeat <- function(eset) length(featureNames(eset))
+    requireID <- function(eset, map) {
+        IDs <- mget(featureNames(eset), envir=getAnnEnv(map))
+        haveID <- names(IDs)[sapply(IDs, function(x) !is.na(x))]
+         eset[haveID, ]
+    }
+    if (require.entrez)
+        eset <- requireID(eset, "ENTREZID")
+
+    filterGO <- function(eset, ontology) {
+        haveGo <- sapply(mget(featureNames(eset), getAnnEnv("GO")),
+                         function(x) {
+                             if (length(x) == 1 && is.na(x))
+                                 FALSE
+                             else {
+                                 onts <- subListExtract(x, "Ontology", simplify=TRUE)
+                                 ontology %in% onts
+                             }
+                         })
+        eset[haveGo, ]
+    }
+    
+    if (require.GOBP) 
+        eset <- filterGO(eset, "BP")
+    if (require.GOCC) 
+        eset <- filterGO(eset, "CC")
+    if (require.GOMF) 
+        eset <- filterGO(eset, "MF")
+
+    if (length(feature.exclude)) {
+        fnms <- featureNames(eset)
+        badIdx <- integer(0)
+        for (pat in feature.exclude) {
+            if (nchar(pat) == 0)
+                next
+            badIdx <- c(grep(pat, fnms), badIdx)
+        }
+        if (length(badIdx)) {
+            badIdx <- unique(badIdx)
+            eset <- eset[-badIdx, ]
+        }
+    }
+    if (remove.dupEntrez ) {
+     ## Reduce to unique probe <--> gene mapping here by keeping largest IQR
+     ## We will want "unique genes" in the non-specific filtered gene
+     ## set.
+        uniqGenes <- findLargest(featureNames(eset), rowIQRs(eset),
+                                 annotation(eset))
+        eset <- eset[uniqGenes, ]
+    }
+    eset
+}
+
+
 setMethod("nsFilter", "ExpressionSet",
           function(eset,
                    require.entrez=TRUE,
@@ -79,6 +168,10 @@ setMethod("nsFilter", "ExpressionSet",
 
               if (var.filter) {
                   esetIqr <- apply(exprs(eset), 1, var.func)
+                  ##note this was not happening in the first
+                  ##version - despite the documentation
+                  if( 0 < var.cutoff && var.cutoff < 1 )
+                      var.cutoff = quantile(esetIqr, var.cutoff)
                   selected <- esetIqr > var.cutoff
                   eset <- eset[selected, ]
                   logvar <- "numLowVar"
