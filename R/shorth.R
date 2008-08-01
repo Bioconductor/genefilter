7ee1f6e0fc4b4277eb6c29745c982e2fa37027c9
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2008-08-01 00:25:10 +0000

    Better error message from "shorth" if NA/NaN values are passed and na.rm is not TRUE.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genefilter@32988 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/shorth.R
+++ R/shorth.R
@@ -1,39 +1,48 @@
 shorth <- function(x, na.rm=FALSE, tie.action="mean", tie.limit=0.05) {
   stopifnot(is.numeric(x))
-  if (na.rm)
+  if (na.rm) {
     x <- x[is.finite(x)]
-
-  rv <- if(length(x)==0) {
-    as.numeric(NA)
   } else {
-    sx    <- sort(x)
-    width <- round(0.5*length(x))
-    diffs <- sx[(width+1):length(x)] - sx[1:(length(x)-width)]
-
-    ## cannot use which.min since we want all minimising points not just one:
-    q     <- which(diffs==min(diffs))
-
-    if(length(q)>1) {
-      ## deal with ties:
-      maxq = max(q)
-      minq = min(q)
-      ## take the action specified in "tie.action"
-      q <- switch(tie.action,
-                  mean = {
-                    if (maxq-minq <= tie.limit * length(x)) {
-                      mean(q)
-                    } else {
-                      stop(paste("Encountered a tie, and the difference between minimal and maximal value is larger than 'length(x)*tie.limit'.",
-                                 "This could mean that the distribution does not have a single well-defined mode.",
-                                 paste("q=", minq, "...", maxq, ",  values=", signif(sx[minq],4), "...", signif(sx[minq+width],4), sep=""), sep="\n"))
-                    }},
-                  max  = maxq, ## largest midpoint (maxq)
-                  min  = minq, # smallest midpoint (minq)
-                  stop(sprintf("Invalid value '%s' for argument 'tie.action'", tie.action))
-                  )
-    }
-    mean(sx[q:(q+width-1)])
+    if(any(!is.finite(x)))
+      stop("'x' contains NA or NaN, and 'na.rm' is FALSE.")
   }
-  return(rv)
+    
+  if(length(x)==0L) {
+    
+    NA_real_
+      
+    } else {
+      
+      sx    <- sort(x)
+      width <- round(0.5*length(x))
+      diffs <- sx[(width+1):length(x)] - sx[1:(length(x)-width)]
+      
+      ## cannot use which.min since we want all minimising points not just one:
+      q  <- which(diffs==min(diffs))
+      
+      if(length(q)>1) {
+        ## deal with ties:
+        maxq = max(q)
+        minq = min(q)
+        ## take the action specified in "tie.action"
+        q <- switch(tie.action,
+                    mean = {
+                      if (maxq-minq <= tie.limit * length(x)) {
+                        mean(q)
+                      } else {
+                        stop(paste("Encountered tie(s), and the difference between minimal and maximal value is larger than 'length(x)*tie.limit'.",
+                                   "This could mean that the distribution does not have a single well-defined mode.",
+                                   paste("q=", minq, "...", maxq, ",  values=", signif(sx[minq],4), "...", signif(sx[minq+width],4), sep=""), sep="\n"))
+                      }},
+                    max  = maxq, ## largest midpoint (maxq)
+                    min  = minq, ## smallest midpoint (minq)
+                    stop(sprintf("Invalid value '%s' for argument 'tie.action'", tie.action))
+                    )
+      } ## if
+      
+      mean(sx[q:(q+width-1)])
+      
+    } ## if
+  
 }
 
