50827646709cee10426a28c43021f331262da724
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2013-03-14 21:05:27 +0000

    More efficient 'dist2' function following a suggestion by James Reid.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genefilter@74347 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/dist2.R
+++ R/dist2.R
@@ -1,12 +1,21 @@
-dist2 = function(x, fun=function(a,b) mean(abs(a-b), na.rm=TRUE), diagonal=0) {
-  if(!(is.numeric(diagonal)&&(length(diagonal)==1L)))
+dist2 = function (x,
+                  fun = function(a, b) mean(abs(a - b), na.rm = TRUE),
+                  diagonal = 0) {
+
+  if (!(is.numeric(diagonal) && (length(diagonal) == 1)))
     stop("'diagonal' must be a numeric scalar.")
-  res = matrix(diagonal, ncol=ncol(x), nrow=ncol(x))
+
+  if (missing(fun)) {
+    res = apply(x, 2, function(w) colMeans(abs(x-w), na.rm=TRUE))
+  } else {
+    res = matrix(diagonal, ncol = ncol(x), nrow = ncol(x))
+    if (ncol(x) >= 2) {
+      for (j in 2:ncol(x))
+        for (i in 1:(j - 1))
+          res[i, j] = res[j, i] = fun(x[, i], x[, j])
+    } # if
+  } # else
+
   colnames(res) = rownames(res) = colnames(x)
-  if(ncol(x)>=2) {
-    for(j in 2:ncol(x))
-      for(i in 1:(j-1))
-        res[i, j] = res[j, i] = fun(x[,i], x[,j])
-  }
   return(res)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,5 @@
 .onLoad <- function(lib, pkgname) {
-    require("methods", quietly=TRUE) || stop("methods package not found")
-    
-    if(.Platform$OS.type == "windows" && require("Biobase") && interactive()
-        && .Platform$GUI ==  "Rgui"){
+    if(.Platform$OS.type == "windows" && interactive() && .Platform$GUI ==  "Rgui"){
       addVigs2WinMenu("genefilter")
    }
  }
